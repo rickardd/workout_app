967131573f1efdae9dd4eae5725301086a2e36aa
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2018-04-16 23:27:40 +1200

    vue proxy

--- vue/src/services/Api.js
+++ vue/src/services/Api.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:3001/`
+    // baseURL: `http://localhost:3001/`
+    baseURL: `/`
   })
 }
