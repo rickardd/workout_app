8dc389e0431a0ade3127b2d2c1eb907f1a869103
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2018-04-19 21:45:38 +1200

    added google fonts

--- vue/src/services/Api.js
+++ vue/src/services/Api.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
 export default () => {
+  const URL = window.location.hostname === "localhost"
+    ? 'http://localhost:3001/'
+    : '/'
+
   return axios.create({
-    // baseURL: `http://localhost:3001/`
-    baseURL: `/`
+    baseURL: URL
   })
 }
